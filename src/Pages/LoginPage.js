8c35aaf2f3f2c979fc19aa273ac90e454804a7e3
Hyunseung Lim <charlie9807@kaist.ac.kr>
2023-07-11 00:15:18 +0900

    add user id, add answer all question, add progress bar

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './page.css';
 
 
 export const LoginPage = (props) => {
+
+    const [id, setId] = useState('');
+
+    const inputHandler = (e) => {
+        e.preventDefault();
+        setId(e.target.value);
+    }
+
     return(
         <div className='page'>
             <div className='loginContainer'>
                 <div className='title'>
                     User ID
                 </div>
-                <input></input>
-                <Link to='/intro' className='loginBtn'>Login</Link>
+                <input onChange={inputHandler}></input>
+                <Link to={id === '' ? '' : '/intro'} state={{id: id}} className='loginBtn'>Login</Link>
+            </div>
+            <div className='loginDescription'>
+                This interface is suitable for desktop.
             </div>
             <div className='footer'/>
         </div>
