c11246be0a745dc9ab79db8f7ae4ff9ff0105801
Yoojin Hong <dbwk18@kaist.ac.kr>
2023-08-21 01:49:44 +0900

    Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAyURkO684YYQb00PEhP-uUlAtaemjelT8",
-  authDomain: "empathetic-proxy-s2c2-feminist.firebaseapp.com",
-  databaseURL: "https://empathetic-proxy-s2c2-feminist-default-rtdb.firebaseio.com",
-  projectId: "empathetic-proxy-s2c2-feminist",
-  storageBucket: "empathetic-proxy-s2c2-feminist.appspot.com",
-  messagingSenderId: "698383867503",
-  appId: "1:698383867503:web:a20291c6e177440bc8f79a",
-  measurementId: "G-1T6L7C4D8P"
+  apiKey: "AIzaSyA9tsIY7cHq9teBMEDCgI9MqV3od3Nxq5c",
+  authDomain: "empathetic-proxy-s1-abortion2.firebaseapp.com",
+  databaseURL: "https://empathetic-proxy-s1-abortion2-default-rtdb.firebaseio.com",
+  projectId: "empathetic-proxy-s1-abortion2",
+  storageBucket: "empathetic-proxy-s1-abortion2.appspot.com",
+  messagingSenderId: "640620794453",
+  appId: "1:640620794453:web:af71fc88f087ad4e773adc",
+  measurementId: "G-Q5D7RE90MQ"
 };
 
 // Initialize Firebase
